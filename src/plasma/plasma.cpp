b7108f98c458b6af6406e6e0dbe032c93b1ac85a
Dru Still <eugene_still@brown.edu>
2025-12-31 18:41:16 -0500

    Window class

--- src/plasma/plasma.cpp
+++ src/plasma/plasma.cpp
@@ -1,45 +1,11 @@
 #include "../../include/shader.h"
-#include <cstddef>
-#include <ostream>
+#include "../window.cpp"
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
 int main() {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Plasma", NULL, NULL);
-  if (window == NULL) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-
-  glViewport(0, 0, 800, 600);
-
+  Window window(800, 600, "Plasma");
   Shader ourShader("src/plasma/plasma.vs", "src/plasma/plasma.fs");
 
   float vertices[] = {
@@ -58,9 +24,7 @@ int main() {
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  while (!glfwWindowShouldClose(window)) {
-    processInput(window);
-
+  while (!window.shouldClose()) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     ourShader.use();
@@ -68,12 +32,11 @@ int main() {
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 6);
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    window.swapBuffers();
+    window.pollEvents();
   }
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
-  glfwTerminate();
   return 0;
 }
