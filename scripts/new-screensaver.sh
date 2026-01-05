82e8b3be31f5c7078e04215bdeec698943990d27
Dru Still <eugene_still@brown.edu>
2026-01-05 10:19:22 -0500

    Template for new screensavers

--- scripts/new-screensaver.sh
+++ scripts/new-screensaver.sh
@@ -10,6 +10,61 @@ if [ -z "$name" ]; then
   exit 1
 fi
 
+classname="$(tr '[:lower:]' '[:upper:]' <<< ${name:0:1})${name:1}"
+uppername=$(echo "$name" | tr '[:lower:]' '[:upper:]')
+
 mkdir src/$name
-touch src/$name/$name.cpp src/$name/$name.h src/$name/$name.vs src/$name/$name.fs src/$name/macos.mm src/$name/main.cpp
+
+write_main () {
+  cat > "src/$name/main.cpp" << EOF
+#include "../../include/glfw_runner.h"
+#include "$name.h"
+
+int main() { return runWithGLFW<$classname>(800, 600, "$classname", "src/$name/"); }
+EOF
+}
+
+write_h () {
+  cat > "src/$name/$name.h" << EOF
+#ifndef ${uppername}_H
+#define ${uppername}_H
+
+#include "../../include/screensaver.h"
+#include "../../include/shader.h"
+
+class $classname : public Screensaver {
+public:
+  void init(int width, int height, const std::string &resourcePath) override;
+  void resize(int width, int height) override;
+  void update(float deltaTime, float totalTime) override;
+  void render() override;
+  void cleanup() override;
+
+private:
+  Shader *shader = nullptr;
+  unsigned int VAO = 0, VBO = 0;
+  float time = 0.0f;
+};
+
+#endif
+EOF
+}
+
+write_mm () {
+  cat > "src/$name/macos.mm" << EOF
+#include <glad/glad.h>
+#include <string>
+
+#include "../backends/SaverTemplate.h"
+#include "$name.h"
+
+SAVER_TEMPLATE(${classname}SaverView, $classname)
+EOF
+}
+
+touch src/$name/$name.cpp src/$name/$name.vs src/$name/$name.fs 
+write_main
+write_h
+write_mm
+
 echo "Initialized $name"
